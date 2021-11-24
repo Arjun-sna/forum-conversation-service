2ea734f7a3d652672f27d98a4d1deb02f2f00fe4
Arjun <arj.sna@gmail.com>
2021-11-24 19:14:50 +0530

    refactor app and server

---
+++ app/index.ts
@@ -0,0 +1,24 @@
+import express, { Application } from "express";
+import cors from "cors";
+import http from "http";
+import logger from "./utils/logger";
+import { initRoutes } from "./routes";
+import initializeSentry from "./sentry";
+import { assertDatabaseConnectionOk } from "./db";
+import exceptionHandler from "./middlewares/exceptionHandler";
+import invalidRouteHandler from "./middlewares/invalidRouteHandler";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+initializeSentry();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(require("method-override")());
+initRoutes(app);
+app.use(exceptionHandler);
+app.use(invalidRouteHandler);
+
+export default app;
--- server.ts
+++ server.ts
@@ -1,25 +1,10 @@
-import express, { Application } from "express";
-import cors from "cors";
+import { Application } from "express";
 import http from "http";
 import logger from "./app/utils/logger";
-import { initRoutes } from "./app/routes";
-import initializeSentry from "./sentry";
 import { assertDatabaseConnectionOk } from "./app/db";
-import exceptionHandler from "./app/middlewares/exceptionHandler";
-import invalidRouteHandler from "./app/middlewares/invalidRouteHandler";
+import app from "./app";
 
 const port = process.env.PORT || 3000;
-const app = express();
-
-initializeSentry();
-
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(require("method-override")());
-initRoutes(app);
-app.use(exceptionHandler);
-app.use(invalidRouteHandler);
 
 async function init(app: Application) {
   await assertDatabaseConnectionOk();
