e7a25d600930bfe54691bf7c51a6b04e5f361957
Arjun <arj.sna@gmail.com>
2021-11-25 22:10:18 +0530

    fix db code

--- app/db/message.ts
+++ app/db/message.ts
@@ -1,28 +1,35 @@
 import { DataTypes, Sequelize } from "sequelize";
 
 export default (sequelize: Sequelize) => {
-  sequelize.define("Message", {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
+  sequelize.define(
+    "Message",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      conversation_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      message: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      date_created: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
     },
-    conversation_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    message: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    date_created: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-  });
+    {
+      tableName: "messages",
+      timestamps: false,
+    }
+  );
 };
