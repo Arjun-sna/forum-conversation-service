22cd18f5c2575894fcc83dfb4eb1423fdbe7e7fb
Arjun <arj.sna@gmail.com>
2021-11-26 19:18:41 +0530

    add get conv route

--- app/routes/v1/index.ts
+++ app/routes/v1/index.ts
@@ -1,4 +1,7 @@
-import { createConversation } from "../../controllers/conversation";
+import {
+  createConversation,
+  getConversations,
+} from "../../controllers/conversation";
 import { Router } from "express";
 import { getHealth } from "../../controllers/health";
 import { catchAsyncController } from "../../utils/helpers";
@@ -8,5 +11,6 @@ const router = Router();
 router.get("/health", catchAsyncController(getHealth));
 
 router.post("/conversation", catchAsyncController(createConversation));
+router.get("/conversation", catchAsyncController(getConversations));
 
 export default router;
