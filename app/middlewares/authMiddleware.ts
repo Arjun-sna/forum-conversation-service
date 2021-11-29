b1f7ff78de5095b479a93480544b0804f15e3465
Arjun <arj.sna@gmail.com>
2021-11-29 22:18:50 +0530

    authenticate

--- app/middlewares/authMiddleware.ts
+++ app/middlewares/authMiddleware.ts
@@ -11,6 +11,12 @@ export default async function (
   res: Response,
   next: NextFunction
 ) {
+  /**
+   * Auth service to be used
+   * decode token with key
+   * map against local database
+   * return the user
+   */
   const currentUser = await User.findOne({ where: { id: 1 } });
   req.currentUser = currentUser as UserModel;
   next();
