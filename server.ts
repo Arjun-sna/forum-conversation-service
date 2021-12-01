b27fca32ef33f54ff939f5d566717e2f4ac1b252
Arjun <arj.sna@gmail.com>
2021-12-01 09:08:59 +0530

    add config setup

--- server.ts
+++ server.ts
@@ -3,8 +3,9 @@ import http from "http";
 import logger from "./app/utils/logger";
 import { assertDatabaseConnectionOk } from "./app/db";
 import app from "./app";
+import config from "./app/config";
 
-const port = process.env.PORT || 3000;
+const port = config.port || 3000;
 
 async function init(app: Application) {
   await assertDatabaseConnectionOk();
